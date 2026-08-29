cffd171d8e913f7a4875d6e3b8841fb8b48765b4
Keshav Pawar <keshavpawar784@gmail.com>
2026-08-30 00:14:14 +0530

    feat:add student's freedom plans feature

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,10 +1,12 @@
 import Hero from "./sections/Hero.tsx";
 import EmotionalHungerTimeline from "./sections/EmotionalHungerTimeline.tsx";
-export function LandingPage(){
+import StudentFreedom from "./sections/StudentFreedom.tsx";
+export function LandingPage() {
     return (
         <>
-      <Hero />
-      <EmotionalHungerTimeline/>
+            <Hero />
+            <EmotionalHungerTimeline />
+            <StudentFreedom />
         </>
 
     );
