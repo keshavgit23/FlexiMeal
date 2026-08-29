38a1349a00316fef831a94fc8a28a4855b06f2ae
Keshav Pawar <keshavpawar784@gmail.com>
2026-08-29 23:43:54 +0530

    feat: HumgerTimeLine with timeline cards

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,8 +1,10 @@
 import Hero from "./sections/Hero.tsx";
+import EmotionalHungerTimeline from "./sections/EmotionalHungerTimeline.tsx";
 export function LandingPage(){
     return (
         <>
       <Hero />
+      <EmotionalHungerTimeline/>
         </>
 
     );
